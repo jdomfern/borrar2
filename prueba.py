32d7a23c8cbba9ddd80d27c8667c17a5389b87ae
jdomfern <jdomfer870@g.educaand.es>
2024-10-21 17:05:46 +0200

    new file:   holamundo.py

---
+++ prueba.py
@@ -0,0 +1,13 @@
+print ("hello world")
+print ("hola mundo")
+a=input ("¿Como te llamas?")
+#comentario
+#print (f"Hola {a}")
+
+'''comentario
+derewr
+rfew'''
+
+for i in range (0,10):
+    print(i)
+print("FIN")
